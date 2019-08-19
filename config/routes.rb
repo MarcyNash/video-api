48aec8deba6630310cd4e5097b699869b9c75f36
MarcyNash <marcywsa@gmail.com>
2019-08-19 09:57:52 -0400

    Fixed bug in index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'videos/index'
+  # get 'videos/index'
+  resources :videos,         only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
