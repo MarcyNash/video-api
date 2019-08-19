ef2227d734fd290df990fead0b6ec833db74e0d3
MarcyNash <marcywsa@gmail.com>
2019-08-19 09:55:53 -0400

    Added search by song and removed debug puts

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,17 @@
 class VideosController < ApplicationController
+  # GET /videos
   def index
+    puts(request.params)
+    if params[:artist]
+      @videos = Video.where(artist_title: params[:artist])
+    elsif params[:song]
+      @videos = Video.where(song_title: params[:song])
+    elsif params[:city]
+      @videos = Video.where(city_title: params[:city])
+    else
+      @videos = Video.all
+    end
+
+    render json: @videos
   end
 end
